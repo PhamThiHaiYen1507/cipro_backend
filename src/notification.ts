52af47842c08eb9c988abd76aa732b051c9d0f24
phamthihaiyen <yen.pth1507@gmail.com>
2024-12-13 08:04:54 +0700

    add firebase service

--- src/notification.ts
+++ src/notification.ts
@@ -1,13 +1,17 @@
+import * as dotenv from "dotenv";
 import admin from "firebase-admin";
-import { firebaseService } from "./firebase/firebaseService";
+import fs from "fs";
+import path from "path";
+dotenv.config();
 
+const serviceAccountPath = path.resolve(__dirname, './firebase/firebase-service.json');
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+
+serviceAccount.private_key = process.env.FIREBASE_NOTIFICATION_PRIVATE_KEY;
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: firebaseService.project_id,
-        clientEmail: firebaseService.client_email,
-        privateKey: firebaseService.private_key
-    }),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
