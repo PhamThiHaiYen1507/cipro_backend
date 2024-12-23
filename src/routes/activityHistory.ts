9c83360b6d457f4a974c9164a788400393af10d5
phamthihaiyen <yen.pth1507@gmail.com>
2024-12-23 17:56:44 +0700

    format code

--- src/routes/activityHistory.ts
+++ src/routes/activityHistory.ts
@@ -4,5 +4,6 @@ import { checkAuth } from "../middlewares/auth";
 
 const activityHistoryRoute = express.Router();
 activityHistoryRoute.get("/:projectName", checkAuth, getActivityHistory);
+
 activityHistoryRoute.get("/scan", checkAuth, getScanHistory);
 export default activityHistoryRoute;
